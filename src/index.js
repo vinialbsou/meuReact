7185c5da3f162a664eff3f7c6610de981f1a1466
root <root@LAPTOP-EGJI61GV>
2024-03-28 10:37:53 -0300

    add login page and top menu

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// ReactDOM.render(
+//     <h1>Hello World</h1>, document.getElementById('root')
+// );
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
